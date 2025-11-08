314284aa7dd4b534de04b9682ec991c77586cc6c
Aseel Al Dallal <aseelaldallal@gmail.com>
2025-11-07 16:09:48 -0800

    prettier formatting

--- src/activities.ts
+++ src/activities.ts
@@ -1,47 +1,43 @@
-
-
-
-
 export const readAseelResume = async (): Promise<string> => {
-    console.log("Reading Aseel's Resume");
-    console.log("She's a cool human");
-    console.log("She has relevant skills");
-    console.log("She has valuable experience");
-    return 'Resume reviewed: She has potential! Lets interview'
-}
+  console.log("Reading Aseel's Resume");
+  console.log("She's a cool human");
+  console.log('She has relevant skills');
+  console.log('She has valuable experience');
+  return 'Resume reviewed: She has potential! Lets interview';
+};
 
 export const meetAndGreetAseel = async (): Promise<string> => {
-    console.log('talk talk talk cool things good things');
-    console.log('Wait a minute she has bears roaming around her backyard????');
-    console.log('Bobcats and cougars too, her neighbor captured it');
-    console.log('https://www.youtube.com/shorts/vZM6QMXuZ8M')
-    return 'Passed interview!'
-}
+  console.log('talk talk talk cool things good things');
+  console.log('Wait a minute she has bears roaming around her backyard????');
+  console.log('Bobcats and cougars too, her neighbor captured it');
+  console.log('https://www.youtube.com/shorts/vZM6QMXuZ8M');
+  return 'Passed interview!';
+};
 
 export const technicalInterviewOne = async (): Promise<string> => {
-    console.log('some practical coding question non leet code style');
-    return 'Passed!'
-}
+  console.log('some practical coding question non leet code style');
+  return 'Passed!';
+};
 
 export const technicalInterviewTwo = async (): Promise<string> => {
-    console.log('some system design question or something funny');
-    return 'Passed!'
-}
+  console.log('some system design question or something funny');
+  return 'Passed!';
+};
 
 export const rallyEvaluatesAseel = async (): Promise<string> => {
-    console.log('should we hire her?')
-    return '<Insert Rally Evaluation>'
-}
+  console.log('should we hire her?');
+  return '<Insert Rally Evaluation>';
+};
 
 export const aseelEvaluatesRally = async (): Promise<string> => {
-    console.log('Aseel checks out Rally...');
-    console.log('✓ Cool product');
-    console.log('✓ Interesting problems to solve');
-    return 'This could be a great fit!';
-}
+  console.log('Aseel checks out Rally...');
+  console.log('✓ Cool product');
+  console.log('✓ Interesting problems to solve');
+  return 'This could be a great fit!';
+};
 
-export const makeMutualDecision = async(): Promise<string> => {
-    console.log('Both sides think this could work...');
-    console.log('Lets have a conversation!');
-    return 'Time to talk!';
-}
+export const makeMutualDecision = async (): Promise<string> => {
+  console.log('Both sides think this could work...');
+  console.log('Lets have a conversation!');
+  return 'Time to talk!';
+};
